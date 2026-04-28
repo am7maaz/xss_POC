d206017e892884b8718f2e692b21d0226b47249a
hamza lachgar <121760220+am7maaz@users.noreply.github.com>
2026-04-28 14:24:02 +0100

    Add redirect to trip.com after 3 seconds
    Redirects the user to a specified URL after a 3-second delay.

---
+++ redirect.js
@@ -0,0 +1,3 @@
+setTimeout(() => {
+  window.location.href = "https://www.trip.com/travel-guide/personal-home/5B0331C7B13B98575B7383FEB36159AB0A22D2851A580B9627C7CF0FB02BD1BB?locale=en_us&curr=USD";
+}, 3000); // 3 seconds
